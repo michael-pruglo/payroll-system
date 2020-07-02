fa003f6e6bbcf5030db6d9fd6095ebd7bac4e418
michael-pruglo <michael.pruglo.work@gmail.com>
2020-07-02 22:16:23 +0300

    initial code commit

---
+++ cmake-build-debug/CMakeFiles/payroll_system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/payroll_system.dir/main.cpp.obj"
+  "libpayroll_system.dll.a"
+  "payroll_system.exe"
+  "payroll_system.exe.manifest"
+  "payroll_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/payroll_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/payroll_system.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for payroll_system.
+# This may be replaced when dependencies are built.
